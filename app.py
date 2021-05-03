8ce5eec4b7a9bd74cf8be33d8773b016aea21fb1
Prakhar Gurunani <prakhargurunani@gmail.com>
2021-05-03 22:45:43 +0530

    depreciated: page titles as URLs in comments; Use raw URLs instead

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import re
 import json
 import os
 
@@ -14,13 +13,13 @@ message_2 = "Oops ! An error occured while processing data. \
 Please follow the guidelines about how to use this bot \
 --> https://github.com/FirePing32/Autolinks"
 
-
+'''
 def get_title(url: str):
     """ Get the Title of the web page and generates markdown formated link"""
     html_source = requests.get(url).text
     title = re.findall('<title>(.*?)</title>', html_source)[0].strip()
     return f"[{title}]({url})"
-
+'''
 
 @app.route("/github/callback", methods=["POST"])
 def issue():
@@ -63,7 +62,7 @@ def issue():
 
             comment_body = message_1 + query + "** - \n\n"
             for site_url in links:
-                comment_body = comment_body + "- " + get_title(site_url) + "\n"
+                comment_body = comment_body + "- " + site_url + "\n"
             comment_body = comment_body + "\n" + "Triggered by @" + user_name
             print("\n" + comment_body)
 
